851ee444691491232e50635c741f381660089723
Lasha Efremidze <efremidzel@hotmail.com>
2017-03-06 15:55:37 -0800

    cleanup

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -14,18 +14,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var button: UIButton!
     
     @IBAction func buttonTapped(_ button: UIButton) {
-        let imageView = UIImageView(image: UIImage(named: "heart"))
-        imageView.tintColor = .red
-        button.animate(customView: imageView)
-        
-//        let label = UILabel()
-//        label.text = "+10pts"
-//        label.textColor = .red
-//        label.font = .boldSystemFont(ofSize: 17)
-//        label.layer.shadowOffset = CGSize(width: 0, height: 1)
-//        label.layer.shadowRadius = 0.1
-//        label.layer.shadowOpacity = 0.1
-//        button.animate(label, direction: .top)
+        switch arc4random_uniform(2) {
+        case 0:
+            let label = UILabel()
+            label.text = "+10pts"
+            label.textColor = .red
+            label.font = .boldSystemFont(ofSize: 17)
+            label.layer.shadowOffset = CGSize(width: 0, height: 1)
+            label.layer.shadowRadius = 0.1
+            label.layer.shadowOpacity = 0.1
+            button.animate(customView: label)
+        default:
+            let imageView = UIImageView(image: UIImage(named: "heart"))
+            imageView.tintColor = .red
+            imageView.layer.shadowOffset = CGSize(width: 0, height: 1)
+            imageView.layer.shadowRadius = 0.1
+            imageView.layer.shadowOpacity = 0.1
+            button.animate(customView: imageView)
+        }
     }
     
 }
